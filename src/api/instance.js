f210f18660c860a8b2f7fc9b9197cc330a7bb3b2
Павел Лютов <cbet0mup.pro@gmail.com>
2021-11-30 20:16:43 +0700

    0055 api

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 export const HTTP = axios.create({
     baseURL: 'http://62.109.8.21/api',
     headers: {
-        post: {
-            'Content-Type': 'application/json'
-        },
+        //post: {
+         //   'Content-Type': 'application/json'
+      //  },
         accept: 'application/json',
-        //mode: 'cors',
+        mode: 'no-cors',
         //header: "Access-Control-Allow-Methods: GET, POST, OPTIONS, HEAD"
     },
 
