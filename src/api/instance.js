11442e4d91f8fc3e5e662ee0953a8a2b997b73ed
Павел Лютов <cbet0mup.pro@gmail.com>
2021-12-10 14:42:27 +0700

    0059 build

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const HTTP = axios.create({
-    //baseURL: 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
+    //baseURL: 'http://localhost:8080/api',
     headers: {
        post: {
             'Content-Type': 'application/json'
