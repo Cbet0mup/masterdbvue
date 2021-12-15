9acd2f59c43c70e34cc5b526eb0e29f172b44e79
Павел Лютов <cbet0mup.pro@gmail.com>
2021-12-16 00:08:04 +0700

    0061 работа над инструкцией

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 export const HTTP = axios.create({
-    //baseURL: 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
+    //baseURL: 'http://localhost:8080/api',
     headers: {
        post: {
             'Content-Type': 'application/json'
         },
-        //'Access-Control-Allow-Origin': 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
+        'Access-Control-Allow-Origin': 'http://xn--80aaabrq9ba6exc.xn--h1ahn.xn--p1acf/api',
         accept: 'application/json',
         mode: 'cors',
         xsrfHeaderName: 'X-XSRF-TOKEN',
